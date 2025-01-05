ea814980b862829209948fefec5228c507cf4578
Vivek Singh Pundir <132738914+vivekdevp01@users.noreply.github.com>
2025-01-05 20:58:53 +0530

    Added new

---
+++ src/utils/common/enum.js
@@ -0,0 +1,16 @@
+const SEAT_TYPE = {
+  BUSINESS: "business",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium-economy",
+  FIRST_CLASS: "first-class",
+};
+const BOOKING_STATUS = {
+  PENDING: "pending",
+  BOOKED: "booked",
+  INITIATED: "initiated",
+  CANCELLED: "canceled",
+};
+module.exports = {
+  SEAT_TYPE,
+  BOOKING_STATUS,
+};
